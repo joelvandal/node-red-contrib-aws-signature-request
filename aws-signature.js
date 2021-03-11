4392235124ea600a54dbdbee287f44f41ac54e7f
Nguyen Hoang <=>
2021-03-11 15:20:00 +0900

    multiple update

--- aws-signature.js
+++ aws-signature.js
@@ -1,58 +1,54 @@
 module.exports = function (RED) {
   "use strict";
   const axios = require('axios');
+  const aws4 = require('aws4');
 
   function AwsSignature(n) {
     RED.nodes.createNode(this, n);
 
-    this.awssignature = RED.nodes.getNode(n.aws);
-    
-    if (!this.aws.credentials.accessToken) {
-      this.status({ fill: "red", shape: "ring", text: "error.no-access-token" });
+    this.aws = RED.nodes.getNode(n.aws);
+
+    if (!this.aws.credentials.accessKey || !this.aws.credentials.secretKey) {
+      this.status({ fill: "red", shape: "ring", text: "error.empty-keys" });
       return;
     }
 
     let node = this;
 
     node.on("input", async function (msg) {
-      let searchKey = msg.search || n.search,
-        useTag = msg.tag || n.tag,
-        sort = msg.sort || n.sort,
-        detailType = msg.detailType || n.detailType,
-        state = msg.state || n.state || "unread";
-
-      let params = {
-        consumer_key: this.aws.credentials.consumerKey,
-        access_token: this.aws.credentials.accessToken,
-        sort,
-        detailType,
-        state
+      let region = msg.region || n.region,
+        service = msg.service || n.service,
+        host = msg.host || n.host,
+        path = msg.path || n.path,
+        method = msg.method || n.method || "GET";
+      node.status({ fill: "blue", shape: "ring", text: "Sending" });
+      let url = host + path;
+      let keys = {
+        accessKeyId: this.aws.credentials.accessKey,
+        secretAccessKey: this.aws.credentials.secretKey,
       }
-
-      if (useTag) {
-        params = { ...params, tag: searchKey }
-      } else {
-        params = { ...params, search: searchKey }
+      let opts = {
+        host: host.replace(/^https?:\/\//g, ''),// remove http|https
+        path,
+        service,
+        region,
+        method
       }
+      // aws4.sign() will sign and modify these options, ready to pass to http.request
+      let config = aws4.sign(opts, keys);
+      config.url = url;
 
-      try {
-        let data = await getList(params);
-
-        msg.payload = data;
-        node.send(msg);
-        node.status({ fill: "green", shape: "ring", text: "success.get-list" });
-      } catch (error) {
-        node.error('Error:', error.response.data)
-        node.status({ fill: "red", shape: "dot", text: "error.get-list" });
-        return;
-      }
+      axios(config)
+        .then(function (response) {
+          msg.payload = response.data;
+          node.send(msg);
+          node.status({ fill: "green", shape: "ring", text: "Success" });
+        })
+        .catch(function (error) {
+          node.error(error.response.data);
+          node.status({ fill: "red", shape: "dot", text: "request.error" });
+        });
     });
   }
   RED.nodes.registerType("aws-signature", AwsSignature);
-
-  async function getList(params) {
-    let { data } = await axios.get("https://getpocket.com/v3/get", { params: params });
-
-    return data;
-  }
 };
